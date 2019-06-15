b38114478b69e14399cb14596db3e0e8df14206e
huihui1 <576902280@qq.com>
2019-06-15 15:17:21 +0800

    [xfs/master] add line_2point_nms_v2

---
+++ include/line_2point_nms_v2.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int test1 (int arg1, float arg2, double arg3, long arg4);
+int test2 (int arg1, float arg2, double arg3, long arg4);
+int test3 (int arg1, float arg2, double arg3, long arg4);
+int test4 (int arg1, float arg2, double arg3, long arg4);
+int test5 (int arg1, float arg2, double arg3, long arg4);
---
+++ src/line_2point_nms_v2.cpp
@@ -0,0 +1,21 @@
+
+int test1 (int arg1, float arg2, double arg3, long arg4)
+{
+	int a = 0;
+}
+int test2 (int arg1, float arg2, double arg3, long arg4);
+{
+	int a = 0;
+}
+int test3 (int arg1, float arg2, double arg3, long arg4);
+{
+	int a = 0;
+}
+int test4 (int arg1, float arg2, double arg3, long arg4);
+{
+	int a = 0;
+}
+int test5 (int arg1, float arg2, double arg3, long arg4);
+{
+	int a = 0;
+}
